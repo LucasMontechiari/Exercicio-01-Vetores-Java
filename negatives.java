8f1436edfd921ea1153cbffab964d1d7a104d718
Lucas Montechiari <110928925+LucasMontechiari@users.noreply.github.com>
2026-02-15 15:58:42 -0300

    Add files via upload

---
+++ negatives.java
@@ -0,0 +1,35 @@
+package Program;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class negatives {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		
+		IO.print("Quantos numeros voce vai digitar?");
+		n = sc.nextInt();
+		
+		int[] vetor = new int[n];
+		
+		for(int i = 0; i<n; i++) {
+			IO.print("Digite um numero: ");
+			vetor[i] = sc.nextInt();
+		}
+		
+		IO.println("NUMEROS NEGATIVOS: ");
+		
+		 for (int i=0; i<n; i++) {
+		        if (vetor[i] < 0) {
+		        	System.out.printf("%d\n", vetor[i]);
+		        }
+		    }
+
+		 sc.close();
+	}
+
+}
